d8fca3d7d68c00c22301acbc190358ef5360bb86
jl-star <57608517+jl-star@users.noreply.github.com>
2024-07-31 15:59:08 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
